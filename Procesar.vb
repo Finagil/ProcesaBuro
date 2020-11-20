b7977016564ccda07a7ecf34b72ad435f15d82d4
ecacerest <ecacerest@finagil.com.mx>
2020-11-20 12:08:36 -0600

    ADD Variables globales para rutas

--- Procesar.vb
+++ Procesar.vb
@@ -1,17 +1,14 @@
 ﻿Imports System.IO
 Module Procesar
-    Dim RutaBuro As String = "\\server-nas\BuroCredito\OCT2019\"
-    Dim RutaBuroBackup As String = "\\server-nas\BuroCredito\OCT2019\Backup\"
-    Dim Mes As String = "20191001"
     Dim TA As New FinagilDSTableAdapters.BC_BuroDatosTableAdapter
     Dim TF As New FinagilDSTableAdapters.FIRA_CALTableAdapter
 
     Sub Main()
 
-        Dim D As New System.IO.DirectoryInfo(RutaBuro)
-        Dim DB As New System.IO.DirectoryInfo(RutaBuroBackup)
+        Dim D As New System.IO.DirectoryInfo(My.Settings.RutaBuro)
+        Dim DB As New System.IO.DirectoryInfo(My.Settings.RutaBuroBackup)
         If Not DB.Exists Then
-            System.IO.Directory.CreateDirectory(RutaBuroBackup)
+            System.IO.Directory.CreateDirectory(My.Settings.RutaBuroBackup)
         End If
         Dim F As FileInfo() = D.GetFiles("*.csv")
         'TA.DeleteALL()
@@ -19,14 +16,14 @@ Module Procesar
         For i As Integer = 0 To F.Length - 1
             Console.WriteLine(F(i).CreationTime & " " & F(i).Name)
             LeeArchivo(F(i).FullName)
-            File.Move(F(i).FullName, RutaBuroBackup & F(i).Name)
+            File.Move(F(i).FullName, My.Settings.RutaBuroBackup & F(i).Name)
         Next
         'PARTE 2++++++++++++++++++++++++++++++++
             F = D.GetFiles("*.txt")
         For i As Integer = 0 To F.Length - 1
             Console.WriteLine(F(i).CreationTime & " " & F(i).Name)
             LeeArchivo2(F(i).FullName)
-            File.Move(F(i).FullName, RutaBuroBackup & F(i).Name)
+            File.Move(F(i).FullName, My.Settings.RutaBuroBackup & F(i).Name)
         Next
         'PARTE 2++++++++++++++++++++++++++++++++
     End Sub
@@ -53,7 +50,7 @@ Module Procesar
                 End If
                 If RFCant <> Datos(4) And RFCant <> "" Then
                     TA.DeleteRFC(RFC)
-                    TA.Insert(RFC, Mes, Vars(0), Vars(1), Vars(2), Vars(3), Vars(4), Vars(5), Vars(6), Vars(7), Vars(8), Vars(9),
+                    TA.Insert(RFC, My.Settings.MES, Vars(0), Vars(1), Vars(2), Vars(3), Vars(4), Vars(5), Vars(6), Vars(7), Vars(8), Vars(9),
                                                     Vars(10), Vars(11), Vars(12), Vars(13), Vars(14), Vars(15), Vars(16), Vars(17), Vars(18), Vars(19),
                                                     Vars(20), Vars(21), Vars(22), Vars(23), Vars(24), Vars(25), Vars(26))
                     X = 0
@@ -73,8 +70,8 @@ Module Procesar
             End If
         End While
         TA.DeleteRFC(RFC)
-        TA.Insert(RFC, Mes, Vars(0), Vars(1), Vars(2), Vars(3), Vars(4), Vars(5), Vars(6), Vars(7), Vars(8), Vars(9), _
-                                        Vars(10), Vars(11), Vars(12), Vars(13), Vars(14), Vars(15), Vars(16), Vars(17), Vars(18), Vars(19), _
+        TA.Insert(RFC, My.Settings.MES, Vars(0), Vars(1), Vars(2), Vars(3), Vars(4), Vars(5), Vars(6), Vars(7), Vars(8), Vars(9),
+                                        Vars(10), Vars(11), Vars(12), Vars(13), Vars(14), Vars(15), Vars(16), Vars(17), Vars(18), Vars(19),
                                         Vars(20), Vars(21), Vars(22), Vars(23), Vars(24), Vars(25), Vars(26))
 
 
@@ -108,8 +105,8 @@ Module Procesar
             Else
                 RFC = Datos(2)
             End If
-            TA.Insert(RFC, Mes, Datos(0 + 5), Datos(1 + 5), Datos(2 + 5), Datos(3 + 5), Datos(4 + 5), Datos(5 + 5), Datos(6 + 5), Datos(7 + 5), Datos(8 + 5), Datos(9 + 5), _
-                                        Datos(10 + 5), Datos(11 + 5), Datos(12 + 5), Datos(13 + 5), Datos(14 + 5), Datos(15 + 5), Datos(16 + 5), Datos(17 + 5), Datos(18 + 5), Datos(19 + 5), _
+            TA.Insert(RFC, My.Settings.MES, Datos(0 + 5), Datos(1 + 5), Datos(2 + 5), Datos(3 + 5), Datos(4 + 5), Datos(5 + 5), Datos(6 + 5), Datos(7 + 5), Datos(8 + 5), Datos(9 + 5),
+                                        Datos(10 + 5), Datos(11 + 5), Datos(12 + 5), Datos(13 + 5), Datos(14 + 5), Datos(15 + 5), Datos(16 + 5), Datos(17 + 5), Datos(18 + 5), Datos(19 + 5),
                                         Datos(20 + 5), Datos(21 + 5), Datos(22 + 5), Datos(23 + 5), Datos(24 + 5), Datos(25 + 5), Datos(26 + 5))
             Console.WriteLine(RFC)
         End While
